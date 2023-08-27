62e90ba53c34529fa6339ddef57dcca54479b734
Gourav Kundu <gouravkundu2902@gmail.com>
2023-08-28 00:45:05 +0530

    Updated the graph Problem MinIsland

--- Graphs/src/NumOfIsland.java
+++ Graphs/src/NumOfIsland.java
@@ -39,7 +39,7 @@ public class NumOfIsland {
     }
 
     public static void main(String[] args) {
-        char[][] grid = {{0,1,1,1,0,0,0},{0,0,1,1,0,1,0}};
+        char[][] grid = {{'0','1','1','1','0','0','0'},{'0','0','1','1','0','1','0'}};
         System.out.println(numIslands(grid));
     }
 }
