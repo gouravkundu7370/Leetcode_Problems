369815b16a0c3f0178a8bed7adc7cb11961fa744
Gourav Kundu <gouravkundu2902@gmail.com>
2023-09-09 20:43:14 +0530

    Updated DetectCycleDirected Graphs

--- Graphs/src/DetectCycleDirectedGraph.java
+++ Graphs/src/DetectCycleDirectedGraph.java
@@ -40,21 +40,63 @@ public class DetectCycleDirectedGraph {
             System.out.println("There exists no cycle in the graph");
         }
     }
+    private static boolean dfsCheck(int node, ArrayList<ArrayList<Integer>> adj, int[] vis, int[] pathVis) {
+        vis[node] = 1;
+        pathVis[node] = 1;
+
+        // traverse for adjacent nodes
+        for(int it : adj.get(node)) {
+            // when the node is not visited
+            if(vis[it] == 0) {
+                if(dfsCheck(it, adj, vis, pathVis))
+                    return true;
+            }
+            // if the node has been previously visited
+            // but it has to be visited on the same path
+            else if(pathVis[it] == 1) {
+                return true;
+            }
+        }
+
+        pathVis[node] = 0;
+        return false;
+    }
+
+    // Function to detect cycle in a directed graph.
+    public static boolean isCyclic(int V, ArrayList<ArrayList<Integer>> adj) {
+        int[] vis = new int[V];
+        int[] pathVis = new int[V];
+
+        for(int i = 0;i<V;i++) {
+            if(vis[i] == 0) {
+                if(dfsCheck(i, adj, vis, pathVis)) return true;
+            }
+        }
+        return false;
+    }
+
 
     public static void main(String[] args)
     {
-        int V = 5;
+        int V = 10;
         ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>(V);
 
         for (int i = 0; i < V; i++)
             adj.add(new ArrayList<Integer>());
 
         addEdge(adj,0, 1);
-        addEdge(adj,4, 1);
+        addEdge(adj,6, 4);
         addEdge(adj,1, 2);
         addEdge(adj,2, 3);
-        addEdge(adj,3, 1);
+        addEdge(adj,3, 4);
+        addEdge(adj,2, 6);
+        addEdge(adj,4, 5);
+        addEdge(adj,7, 1);
+        addEdge(adj,7, 8);
+        addEdge(adj,8, 9);
+        addEdge(adj,9, 7);
 
         topologicalSort(adj,V);
+        System.out.println(isCyclic(V,adj));
     }
 }
